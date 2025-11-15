7a7ced90c958a8a6b3710a9dd21d2feb012ee455
devendra singh goud <devendrasinghgoud4@gmail.com>
2025-11-15 12:58:01 +0530

    Add filter to find task

--- src/routes/task.routes.js
+++ src/routes/task.routes.js
@@ -5,10 +5,10 @@ import * as TaskController from "../modules/task/task.controller.js";
 const router = express.Router();
 
 // Task CRUD routes
-router.post("/", verifyAdmin, TaskController.createTask);          // Create a task
-router.get("/", verifyAdmin, TaskController.getAllTasks);          // Get all tasks
-router.get("/:id", verifyAdmin, TaskController.getTaskById);       // Get single task
-router.put("/:id/status", verifyToken, TaskController.updateTaskStatus); // Update task status
-router.delete("/:id", verifyAdmin, TaskController.deleteTask);     // Delete a task
+router.post("/", verifyAdmin, TaskController.createTask);                
+router.get("/", verifyAdmin, TaskController.getAllTasks);                
+router.get("/:id", verifyAdmin, TaskController.getTaskById);             
+router.put("/:id/status", verifyToken, TaskController.updateTaskStatus); 
+router.delete("/:id", verifyAdmin, TaskController.deleteTask);           
 
 export default router;
